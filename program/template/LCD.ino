106188ec8d26ee0a42b002a30df1d5eacd8fc619
tanukiti41 <takaki_m@outlook.jp>
2019-02-24 15:19:42 +0900

    Create LCD.ino

---
+++ program/template/LCD.ino
@@ -0,0 +1,15 @@
+//初期化
+void _LCD::init(void) {
+  lcd.begin();
+  lcd.command(0x38);
+  lcd.command(0x39);
+  lcd.command(0x14);
+  lcd.command(0x73);
+  lcd.command(0x51);
+  lcd.command(0x6c);
+  lcd.command(0x38);
+  lcd.command(0x01);
+  lcd.command(0x0c);
+  lcd.clear();
+  lcd.home();
+}
