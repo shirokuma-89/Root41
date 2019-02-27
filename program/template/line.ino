9787ff3621b601910188736257b07769e7005603
tanukiti41 <takaki_m@outlook.jp>
2019-02-27 21:34:34 +0900

    Create line.ino

---
+++ program/template/line.ino
@@ -0,0 +1,20 @@
+// line.ino
+
+_line::_line(void) {
+  for (int i = 0; i <= 3; i++) {
+    val[i] = false;
+  }
+}
+
+void _line::read(void) {
+  touch = false;
+
+  val[0] = !digitalRead(LINE1);
+  val[1] = !digitalRead(LINE2);
+  val[2] = !digitalRead(LINE3);
+  val[3] = !digitalRead(LINE4);
+
+  if (val[0] | val[1] | val[2] | val[3]) {
+    touch = true;
+  }
+}
