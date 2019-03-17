7902ed62c04ba4b708a91485e32d6a412aeb6269
akapon41 <kuruhajime@gmail.com>
2019-03-17 14:29:21 +0900

    Update Usonic.ino
    コメントを追加 ’o’

--- program/test/Usonic/Usonic.ino
+++ program/test/Usonic/Usonic.ino
@@ -40,6 +40,7 @@ byte _usonic::read(void) {
   timer = pulseIn(ECHO, HIGH);  // パルス幅を計測
 
   if (timer < 18000) {
+    //距離を計算
     ans = int(((float)timer / 29.0) / 2.0);
   } else {
     // error!
