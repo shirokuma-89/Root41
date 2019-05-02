cc0855025caaebe6dcb3a9875d2c32e292ccc5ed
tanukichi41 <takaki_m@outlook.jp>
2019-05-02 23:05:51 +0900

    テスト用にフォルダ作った

---
+++ program/motor/device.ino
@@ -0,0 +1,164 @@
+//device.ino
+
+void _device::initialize(void) {
+  pinMode(BALL0, INPUT);
+  pinMode(BALL1, INPUT);
+  pinMode(BALL2, INPUT);
+  pinMode(BALL3, INPUT);
+  pinMode(BALL4, INPUT);
+  pinMode(BALL5, INPUT);
+  pinMode(BALL6, INPUT);
+  pinMode(BALL7, INPUT);
+  pinMode(BALL8, INPUT);
+  pinMode(BALL9, INPUT);
+  pinMode(BALL10, INPUT);
+  pinMode(BALL11, INPUT);
+  pinMode(BALL12, INPUT);
+  pinMode(BALL13, INPUT);
+  pinMode(BALL15, INPUT);
+  pinMode(BALL_RESET, OUTPUT);
+
+  pinMode(LINE1, INPUT_PULLUP);
+  pinMode(LINE2, INPUT_PULLUP);
+  pinMode(LINE3, INPUT_PULLUP);
+  pinMode(LINE4, INPUT_PULLUP);
+
+  pinMode(SW_TOGGLE, INPUT);
+  pinMode(SW_RIGHT, INPUT);
+  pinMode(SW_LEFT, INPUT);
+
+  pinMode(ROTARY1, INPUT);
+  pinMode(ROTARY2, INPUT);
+
+  digitalWrite(BALL_RESET, HIGH);
+}
+
+void _device::monitor(void) {
+  if (digitalRead(SW_LEFT) || digitalRead(SW_RIGHT) || monitorBegin) {
+    startTimer5(100);
+    lcd.print("Root41 monitor");
+
+    LED.timer = millis();
+    RGBLED.setBrightness(LED.bright + 30);
+
+    digitalWrite(BALL_RESET, HIGH);
+    
+    while (true) {
+      for (LED.j = 0; LED.j < 256; LED.j++) {
+        RGBLED.begin();
+        for (LED.i = 0; LED.i < RGBLED.numPixels(); LED.i++) {
+          RGBLED.setPixelColor(LED.i, Wheel((LED.i + LED.j) & 255));
+        }
+        RGBLED.show();
+
+        // ball.reset();
+
+        LED.timer = millis();
+        while (millis() - LED.timer <= 20) {
+          gyro.deg = gyro.read();
+
+          // ball.val[0] = analogRead(BALL0);
+          // ball.val[1] = analogRead(BALL1);
+          // ball.val[2] = analogRead(BALL2);
+          // ball.val[3] = analogRead(BALL3);
+          // ball.val[4] = analogRead(BALL4);
+          // ball.val[5] = analogRead(BALL5);
+          // ball.val[6] = analogRead(BALL6);
+          // ball.val[7] = analogRead(BALL7);
+          // ball.val[8] = analogRead(BALL8);
+          // ball.val[9] = analogRead(BALL9);
+          // ball.val[10] = analogRead(BALL10);
+          // ball.val[11] = analogRead(BALL11);
+          // ball.val[12] = analogRead(BALL12);
+          // ball.val[13] = analogRead(BALL13);
+          // ball.val[14] = analogRead(BALL14);
+          // ball.val[15] = analogRead(BALL15);
+          ball.read(ball.val);
+          for (int i = 0; i <= 15; i++) {
+            ball.val[i] = round((float)ball.val[i] / 10.24);
+          }
+
+          Serial.write('R');
+          Serial.write('o');
+          Serial.write('o');
+          Serial.write('t');
+          Serial.write('4');
+          Serial.write('1');
+
+          Serial.write(highByte(int(round(gyro.deg))));
+          Serial.write(lowByte(int(round(gyro.deg))));
+          if (!digitalRead(LINE1)) {
+            Serial.write(1);
+          } else {
+            Serial.write(2);
+          }
+          if (!digitalRead(LINE2)) {
+            Serial.write(1);
+          } else {
+            Serial.write(2);
+          }
+          if (!digitalRead(LINE3)) {
+            Serial.write(1);
+          } else {
+            Serial.write(2);
+          }
+          if (!digitalRead(LINE4)) {
+            Serial.write(1);
+          } else {
+            Serial.write(2);
+          }
+          for (int i = 0; i <= 15; i++) {
+            Serial.write(ball.val[i]);
+          }
+          Serial.write(digitalRead(SW_TOGGLE));
+          Serial.write(digitalRead(SW_RIGHT));
+          Serial.write(digitalRead(SW_LEFT));
+          Serial.write(highByte(device.rotary));
+          Serial.write(lowByte(device.rotary));
+
+          if (Serial.available() > 9) {
+            if (Serial.read() == 'r') {
+              if (Serial.read() == 'o') {
+                if (Serial.read() == 'o') {
+                  if (Serial.read() == 't') {
+                    motor.val[0] = Serial.read();
+                    motor.val[1] = Serial.read();
+                    motor.val[2] = Serial.read();
+                    if (Serial.read() == '4') {
+                      if (Serial.read() == '1') {
+                        motor.val[0] -= 100;
+                        motor.val[1] -= 100;
+                        motor.val[2] -= 100;
+                        if (Serial.read() == 1) {
+                          digitalWrite(BALL_RESET, LOW);
+                        } else {
+                          digitalWrite(BALL_RESET, HIGH);
+                        }
+
+                        motor.directDrive(motor.val);
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    pauseTimer5();
+  }
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return RGBLED.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return RGBLED.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return RGBLED.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
---
+++ program/motor/timer5.ino
@@ -0,0 +1,312 @@
+// timer5.ino
+
+ISR(timer5Event) {
+  pauseTimer5();   //割り込みを無効化
+  enableMillis();  // millis()関数を有効化
+
+  if (device.mode == 0 || device.mode == 1) {  //待機中
+    device.rotaryResult = rotary.process();
+    if (device.rotaryResult == DIR_CW) {
+      device.rotary++;
+    } else if (device.rotaryResult == DIR_CCW) {
+      device.rotary--;
+    }
+
+  } else if (device.mode == 2) {  //動作中
+                                  //ライン処理をここに記述
+    //ライン動作中はline.flagをtrueにしておくこと
+    line.read();
+    if (line.stop) {
+      if (millis() - line.inTimer >= line.stoptime) {
+        line.stop = false;
+      }
+      for (int i = 1; i <= 9; i++) {
+        if (line.logs[i] == 5) {
+          if (line.val[0]) {
+            for (int j = 0; j <= i - 1; j++) {
+              if (line.logs[j] == 0) {
+                break;
+              }
+              if (j == i - 1) {
+                line.logs[i] = 0;
+              }
+            }
+          }
+          if (line.val[1]) {
+            for (int j = 0; j <= i - 1; j++) {
+              if (line.logs[j] == 1) {
+                break;
+              }
+              if (j == i - 1) {
+                line.logs[i] = 1;
+              }
+            }
+          }
+          if (line.val[2]) {
+            for (int j = 0; j <= i - 1; j++) {
+              if (line.logs[j] == 2) {
+                break;
+              }
+              if (j == i - 1) {
+                line.logs[i] = 2;
+              }
+            }
+          }
+          if (line.val[3]) {
+            for (int j = 0; j <= i - 1; j++) {
+              if (line.logs[j] == 3) {
+                break;
+              }
+              if (j == i - 1) {
+                line.logs[i] = 3;
+              }
+            }
+          }
+        }
+      }
+    } else if (line.touch) {
+      line.flag = true;
+
+      //触ってるとき
+      if (line.deg == 1000 && line.outMove == 1000) {
+        //初めて
+        line.stop = true;
+        line.near = true;
+        line.inTimer = millis();
+        line.holdTimer = millis();
+        if (ball.exist == false) {
+          line.deg = 1000;
+        } else if (motor.memory <= 180) {
+          line.deg = motor.memory + 180;
+        } else if (motor.memory > 180) {
+          line.deg = motor.memory - 180;
+        }
+        if (line.val[0]) {
+          line.step = 180;
+          line.highPin = 0;
+          line.logs[0] = 0;
+        } else if (line.val[1]) {
+          line.step = 120;  //ゴールキーパーは65
+          line.highPin = 1;
+          line.logs[0] = 1;
+        } else if (line.val[2]) {
+          line.step = 240;  //ゴールキーパーは295
+          line.highPin = 2;
+          line.logs[0] = 2;
+        } else if (line.val[3]) {
+          line.step = 0;
+          line.highPin = 3;
+          line.logs[0] = 3;
+        }
+        if (line.deg >= 180) {
+          line.deg = map(line.deg, 180, 360, -180, 0);
+        }
+        if (line.step >= 180) {
+          line.step = map(line.step, 180, 360, -180, 0);
+        }
+        //一時的にline.degとline.stepを-180~180にする
+        if (line.deg == 1000) {
+          line.deg = line.step;
+        } else if (abs(line.deg - line.step) <= 70) {
+          line.deg = (line.deg + line.step) / 2;
+        } else {
+          if (line.highPin == 1 || line.highPin == 2) {
+            line.deg = (line.deg + line.step) / 2;
+            if (line.highPin == 1) {
+              if (line.deg <= 0) {
+                line.deg += 180;
+              }
+            } else {
+              if (line.deg >= 0) {
+                line.deg -= 180;
+              }
+            }
+          } else if (line.highPin == 0) {
+            if (line.deg >= 0) {
+              line.deg = 150;
+            } else {
+              line.deg = 210;
+            }
+          } else if (line.highPin == 3) {
+            if (ball.turn == 0) {
+              line.deg = (line.deg + line.step) / 2;
+            } else {
+              line.deg = 0;
+            }
+          }
+        }
+        //戻す
+        if (line.deg <= 0) {
+          line.deg = map(line.deg, 0, -180, 360, 180);
+        }
+        if (line.step <= 0) {
+          line.step = map(line.step, 0, -180, 360, 180);
+        }
+      } else if (line.deg != 1000 && line.outMove == 1000) {
+        //連続
+        if (line.highPin == 1 || line.highPin == 2) {
+          if (line.logs[1] == 0) {
+            if (line.logs[2] == 3) {
+              if (line.highPin == 1) {
+                line.deg = 135;
+              } else if (line.highPin == 2) {
+                line.deg = 225;
+              }
+            } else {
+              if (line.highPin == 1) {
+                line.deg = 90;
+              } else if (line.highPin == 2) {
+                line.deg = 270;
+              }
+            }
+          }
+          if (line.logs[1] == 0) {
+            if (line.logs[2] == 3) {
+              if (line.highPin == 1) {
+                line.deg = 45;
+              } else if (line.highPin == 2) {
+                line.deg = 315;
+              }
+            } else {
+              if (line.highPin == 1) {
+                line.deg = 90;
+              } else if (line.highPin == 2) {
+                line.deg = 270;
+              }
+            }
+          }
+          if (line.logs[0] == 1) {
+            if (line.logs[1] == 0) {
+              if (line.logs[2] == 2) {
+                line.deg = 180;
+              }
+            }
+          } else if (line.logs[1] == 2) {
+            if (line.logs[1] == 0) {
+              if (line.logs[2] == 1) {
+                line.deg = 180;
+              }
+            }
+          }
+          if (millis() - line.holdTimer >= 2000) {
+            if (line.logs[1] == 0) {
+              line.deg = 180;
+            } else if (line.logs[1] == 3) {
+              line.deg = 0;
+            }
+          }
+        }
+        if (line.highPin == 0) {
+          if (millis() - line.holdTimer >= 2000) {
+            line.deg = 180;
+          }
+        }
+        if (line.highPin == 3) {
+          if (line.logs[1] == 1 && line.logs[2] == 0) {
+            line.deg = 50;
+          } else if (line.logs[1] == 2 && line.logs[2] == 0) {
+            line.deg = 330;
+          } else {
+            line.deg = 0;
+          }
+          if (millis() - line.holdTimer >= 2000) {
+            line.deg = 0;
+          }
+        }
+        for (int i = 1; i <= 9; i++) {
+          if (line.logs[i] == 5) {
+            if (line.val[0]) {
+              for (int j = 0; j <= i - 1; j++) {
+                if (line.logs[j] == 0) {
+                  break;
+                }
+                if (j == i - 1) {
+                  line.logs[i] = 0;
+                }
+              }
+            }
+            if (line.val[1]) {
+              for (int j = 0; j <= i - 1; j++) {
+                if (line.logs[j] == 1) {
+                  break;
+                }
+                if (j == i - 1) {
+                  line.logs[i] = 1;
+                }
+              }
+            }
+            if (line.val[2]) {
+              for (int j = 0; j <= i - 1; j++) {
+                if (line.logs[j] == 2) {
+                  break;
+                }
+                if (j == i - 1) {
+                  line.logs[i] = 2;
+                }
+              }
+            }
+            if (line.val[3]) {
+              for (int j = 0; j <= i - 1; j++) {
+                if (line.logs[j] == 3) {
+                  break;
+                }
+                if (j == i - 1) {
+                  line.logs[i] = 3;
+                }
+              }
+            }
+          }
+        }
+      } else if (line.deg == 1000 && line.outMove != 1000) {
+        line.deg = line.outMove;
+        line.outMove = 1000;
+      }
+    } else if (line.flag && line.outMove == 1000) {
+      line.outMove = line.deg;
+      line.deg = 1000;
+      line.outTimer = millis();
+    } else if (line.flag && line.outMove != 1000) {
+      int i;
+      if (line.outMove <= 60 || line.outMove >= 300) {
+        i = 400;
+      } else {
+        i = 300;
+      }
+      if (millis() - line.outTimer >= i) {
+        line.flag = false;
+        line.outMove = 1000;
+      }
+    } else {
+      line.flag = false;
+      line.deg = 1000;
+      line.outMove = 1000;
+      line.step = 1000;
+      for (int i = 0; i <= 9; i++) {
+        line.logs[i] = 5;
+      }
+    }
+  }
+
+  //   motor.restart++;
+
+  //   if (motor.restart >= 2000) {
+  //     motor.restart = 0;
+  //     for (int i = 0; i <= 100; i++) {
+  //       digitalWrite(4, LOW);
+  //       digitalWrite(5, LOW);
+  //       digitalWrite(6, LOW);
+  //       digitalWrite(7, LOW);
+  //       digitalWrite(8, LOW);
+  //       digitalWrite(9, LOW);
+  //       digitalWrite(10, LOW);
+  //       digitalWrite(11, LOW);
+  //       digitalWrite(12, LOW);
+  //     }
+  //     motor.directDrive(motor.val);
+  //   }
+  // }
+  // if (device.mode != 2 || !device.keeper) {
+  startTimer5(50);  //タイマー割り込みを有効化
+                    // }
+  // }
+}
